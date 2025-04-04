69d3d24e26e849fc1285777e0a443ae8fa08057c
AdsRays <ponomarchukpipes@gmail.com>
2025-04-04 10:26:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MetaProfitDashboard from "./MetaProfitDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <MetaProfitDashboard />
+    </div>
+  );
+}
+
+export default App;
