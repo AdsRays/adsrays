b6160e42c837fcfba8665e9dd41c25b53713c602
AdsRays <ponomarchukpipes@gmail.com>
2025-08-23 09:32:51 +0000

    fix(legacy-ui): route AgeBreakdownPanel via metaApi.extra shim (add age stubs)

---
+++ src/lib/metaApi.extra.js
@@ -0,0 +1,9 @@
+export * from './metaApi';
+
+// --- TEMP stubs for legacy-ui build ---
+export async function fetchCampaignAgeInsights(/* accountId, campaignId, accessToken */) {
+  return [];
+}
+export async function fetchCampaignTargetAges(/* accountId, campaignId, accessToken */) {
+  return [];
+}
