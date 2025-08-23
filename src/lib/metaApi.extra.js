d18b6934607ea10699c38ce7c067234676ed381b
AdsRays <ponomarchukpipes@gmail.com>
2025-08-23 09:44:46 +0000

    chore(legacy-ui): drop unused metaApi.extra shim

--- src/lib/metaApi.extra.js
+++
@@ -1,9 +0,0 @@
-export * from './metaApi';
-
-// --- TEMP stubs for legacy-ui build ---
-export async function fetchCampaignAgeInsights(/* accountId, campaignId, accessToken */) {
-  return [];
-}
-export async function fetchCampaignTargetAges(/* accountId, campaignId, accessToken */) {
-  return [];
-}
